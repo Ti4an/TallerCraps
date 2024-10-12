ec301a6c4d2e630ec3bc5bac1ae0e7b54facde7d
Ti4an <sebastian.bucheli@correounivalle.edu.co>
2024-10-12 11:14:14 -0500

    segundo commit

--- src/main/java/org/example/crapsgame/Main.java
+++ src/main/java/org/example/crapsgame/Main.java
@@ -15,5 +15,6 @@ public class Main extends Application  {
     @Override
     public void start(Stage primaryStage) throws IOException {
         GameStage.getInstance();
+        System.out.println("hola");
     }
 }
